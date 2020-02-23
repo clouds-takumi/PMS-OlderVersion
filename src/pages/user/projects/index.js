b6173e9c4b3e1088f28f18f96712dd486e1b6b24
daveToT <1486950363@qq.com>
2020-02-23 15:06:35 +0800

    'project'

--- src/pages/user/projects/index.js
+++ src/pages/user/projects/index.js
@@ -1,22 +1,54 @@
 import { Component } from 'react'
 import s from './index.less'
 import cn from 'classnames'
-import { Icon, Divider, Drawer, Input } from 'antd'
-import { reqProjects } from '../service'
+import { Icon, Divider, Drawer, Input, Select, Tag, message } from 'antd'
+import { reqProjects, reqTags, reqUserInfo, addProject } from '../service'
 
 class Projects extends Component {
     constructor(props) {
         super(props);
         this.state = {
             projects: [],
-            modalFlag: false
+            modalFlag: false,
+            projectName: '',
+            nameTip: false,
+            tags: null,
+            selectedTags: [],
+            userInfo: null
         }
-
         document.getElementsByTagName("title")[0].innerText = '项目列表'
-
     }
 
     goback = () => this.setState({ modalFlag: false })
+
+    handleNameChange = (e) => {
+        let name = e.target.value
+        this.setState({ projectName: name })
+        if (name === '') {
+            this.setState({ nameTip: true })
+        } else {
+            this.setState({ nameTip: false })
+        }
+    }
+
+    handleSelectTag = value => {
+        if (value) {
+            this.setState({ selectedTags: value })
+        }
+    }
+
+    handleCreate = async () => {
+        const { projectName, userInfo, selectedTags } = this.state
+        const tagsStr = selectedTags.join(',')
+        const product = { name: projectName, status: 0, created: userInfo.id, tags: tagsStr }
+        const result = await addProject(product)
+        if (result && result.id) {
+            message.success('创建成功')
+            this.setState({ projectName: '', selectedTags: [] })
+            this.fetchData()
+        }
+        this.setState({ modalFlag: false })
+    }
 
     fetchData = async () => {
         const resData = await reqProjects()
@@ -25,9 +57,14 @@ class Projects extends Component {
         }
     }
 
-    componentDidMount() { this.fetchData() }
+    componentDidMount() {
+        this.fetchData()
+        reqUserInfo().then(res => { this.setState({ userInfo: res }) })
+        reqTags().then(res => this.setState({ tags: res }))
+    }
 
     renderModal = () => {
+        const { projectName, nameTip, tags } = this.state
         return (
             <Drawer
                 title=""
@@ -48,15 +85,23 @@ class Projects extends Component {
                                 <span>填写项目基本信息</span>
                             </div>
                             <div style={{ display: 'flex' }}>
-                                <div style={{ marginRight: '32px' }}>
+                                <div style={{ marginRight: '32px', width: '500px' }}>
                                     <div className={s.item}>
                                         <div className={s.subtitle}>项目名称</div>
-                                        <Input />
-                                        <span className={s.tips}>可以使用中英文、数字、空格组合</span>
+                                        <Input value={projectName} onChange={this.handleNameChange} />
+                                        <span className={s.tips} style={nameTip ? { color: 'red' } : {}}>{nameTip ? '项目名称不能为空' : '可以使用中英文、数字、空格组合'}</span>
                                     </div>
                                     <div className={s.item}>
                                         <div className={s.subtitle}>项目标签</div>
-                                        <Input />
+                                        <Select
+                                            mode="multiple"
+                                            style={{ width: '100%' }}
+                                            onChange={this.handleSelectTag}>
+                                            {tags && tags.map(
+                                                tag => <Select.Option key={tag.id} >
+                                                    <Tag color={tag.color}>{tag.name}</Tag>
+                                                </Select.Option>)}
+                                        </Select>
                                         <span className={s.tips}>建议选择最多不超过三个</span>
                                     </div>
                                     <div className={s.item}>
@@ -68,7 +113,7 @@ class Projects extends Component {
                                                 className={s.text} />
                                         </span>
                                     </div>
-                                    <button className={cn(s.btn, s.leftBtn)}>完成创建</button>
+                                    <button className={cn(s.btn, s.leftBtn)} onClick={this.handleCreate}>完成创建</button>
                                     <button className={s.btn} onClick={this.goback}>取消</button>
                                 </div>
                                 <div className={s.infoPic}>
@@ -123,7 +168,7 @@ class Projects extends Component {
                                             <div className={s.itemImg}>
                                                 <div>img</div>
                                             </div>
-                                            <div className={s.itemName}>示例项目一</div>
+                                            <div className={s.itemName}>{item.name}</div>
                                         </div>
                                     </div>
                                 )
--- src/pages/user/service/index.js
+++ src/pages/user/service/index.js
@@ -4,3 +4,19 @@ export const reqProjects = () => ajax({
     method: 'get',
     url: '/projects'
 }, true)
+
+export const reqTags = () => ajax({
+    method: 'get',
+    url: '/tags'
+}, true)
+
+export const addProject = data => ajax({
+    method: 'post',
+    url: '/project',
+    data
+}, true)
+
+export const reqUserInfo = () => ajax({
+    method: 'get',
+    url: '/userInfo'
+}, true)
